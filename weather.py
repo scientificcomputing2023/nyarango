540e6797ed8f6fa885a3abcb48b4af9284c10325
magvtv <magutujr@gmail.com>
2023-03-28 07:10:50 +0300

    addedd secant.tex group task

--- weather.py
+++ weather.py
@@ -1,31 +1,36 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
-titles = ["date", "mean_temp"]
-data = pd.read_csv("/home/pharoh/Desktop/LEARN/PY/Data/ldn_weather.csv", usecols = titles) 
-ldn_data = pd.DataFrame(data)
-values = ldn_data.tail(16)
+# titles = ["date", "mean_temp"]
+# data = pd.read_csv("/home/pharoh/Desktop/LEARN/PY/Data/ldn_weather.csv", usecols = titles) 
+# ldn_data = pd.DataFrame(data)
+# values = ldn_data.tail(16)
 
-x = (values["date"] - 20201200)
-y = values["mean_temp"]
+# x = (values["date"] - 20201200)
+# y = values["mean_temp"]
 
-#find line of best fit
-a, b = np.polyfit(x, y, 1)
-plt.title("London Temperatures", fontweight="bold")
+# #find line of best fit
+# a, b = np.polyfit(x, y, 1)
+# plt.title("London Temperatures", fontweight="bold")
 
-#add points to plot
-plt.scatter(x, y)
-plt.xlabel("Date 2020", fontweight="semibold")
-plt.ylabel("Temperature", fontweight="semibold")
-line_eq = ((a * x) + b)
+# #add points to plot
+# plt.scatter(x, y)
+# plt.xlabel("Date 2020", fontweight="semibold")
+# plt.ylabel("Temperature", fontweight="semibold")
+# line_eq = ((a * x) + b)
 
-#add line of best fit to plot
-plt.plot(x, line_eq, ':k')
-plt.show()
+# #add line of best fit to plot
+# plt.plot(x, line_eq, ':k')
+# plt.show()
 
-#lowest temperature from ldn_data
-lowest_temp = min(y)
-print("Lowest Temp: " + str(lowest_temp) + "°C")
+# #lowest temperature from ldn_data
+# lowest_temp = min(y)
+# print("Lowest Temp: " + str(lowest_temp) + "°C")
 
+
+x = range(1, 5, 2)
+for i in x:
+    print("This is number ", i)
+    i+=1
